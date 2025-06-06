b98b7441f778e5cd49c2acf47378396bfbbaf19b
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-06-06 16:15:51 -0300

    Calcular média

--- ex_06.js
+++ ex_06.js
@@ -1,10 +1,10 @@
-function calcularMedia(arr){
+function calcularMedia(arr) {
     let media = 0;
     let soma = 0;
-    for(let i = 0; i < arr.length ; i++){
+    for (let i = 0; i < arr.length; i++) {
         soma += arr[i];
         media = soma / arr.length;
     }
     return media;
 }
-console.log(calcularMedia([2, 3, 3, 5, 7,10]));
+console.log(calcularMedia([2, 3, 3, 5, 7, 10]));
