ab32287608dd5ec4a98443d0480b1f1a10cb9d63
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-06-06 16:03:07 -0300

    Função calcularMedia implementada e testada com um array de exemplo.

--- ex_06.js
+++ ex_06.js
@@ -2,8 +2,8 @@ function calcularMedia(arr){ // função que recebe um array e retorna a média 
     let media = 0;
     let soma = 0;
     for(let i = 0; i < arr.length ; i++){ // percorre o array
-        soma += arr[i] // soma os valores
-        media = soma / arr.length // calcula a média
+        soma += arr[i]; // soma os valores
+        media = soma / arr.length; // calcula a média
     }
-    return media // retorna a média
+    return media; // retorna a média
 }
