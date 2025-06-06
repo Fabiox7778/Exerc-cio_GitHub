476b825e8d3ef3d9619623dd68a4c3eba3f1ef2d
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-06-06 16:00:31 -0300

    Soma o array

--- ex_01.js
+++ ex_01.js
@@ -1,9 +1,9 @@
 function somaArray(array) {
   let soma = 0;
-  for (let i = 0; i < array.length; i++){
-    soma += array [i];
+  for (let i = 0; i < array.length; i++) {
+    soma += array[i];
   }
   return soma;
 }
 
-console.log(somaArray([12, 542, 2, 643, 2]))
+console.log(somaArray([12, 542, 2, 643, 2]));
