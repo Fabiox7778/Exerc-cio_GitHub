4d78fde51c766586874b29926493390f226fd651
Jvsilvagomes <joao.silvassenai@gmail.com>
2025-06-06 15:43:16 -0300

    Invertendo o array

---
+++ ex_04.js
@@ -0,0 +1,11 @@
+function inverterArray(array){
+    let arrayRevertida = []
+    for (let i = array.length - 1; i >= 0; i--){
+        arrayRevertida.push(array[i])
+    }
+    return arrayRevertida;
+}
+let array = [10, 20, 40, 60, 80];
+let arrayRevertida = inverterArray(array);
+console.log(array);
+console.log(arrayRevertida);
